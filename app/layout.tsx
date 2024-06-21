fc973633bae46598af6689f092de7b171143fbb0
alhakeemkabiru <alhakeemkabiru@gmail.com>
2024-06-21 23:02:02 +0100

    dark-mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
-import type { Metadata } from "next";
+"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Ecommerce store",
-  description: "An hardware and electronic gadget ecommerce store",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>
+      <body
+        className={`${inter.className} ${
+          isDarkMode
+            ? "bg-black text-white"
+            : "bg-white text-black transition-colors duration-700"
+        }`}
+      >
+        <button
+          onClick={() => setIsDarkMode(!isDarkMode)}
+          className={`${
+            isDarkMode ? "bg-white text-black" : "bg-black text-white"
+          } border rounded-lg p-4`}
+        >
+          {isDarkMode ? "Light☀️" : "Dark🌙"}
+        </button>
         {children}
       </body>
     </html>
